d4b806d639365ff811ac87e3a93121f7c58cbe6e
Anne Marie Jaminet <annemarie.jaminet@gmail.com>
2021-11-06 16:34:07 +0100

    Saturday afternoon :)

--- Homework 1/SIS_21-22_hwk1_material/material_hw1/part02/filter_data.m
+++ Homework 1/SIS_21-22_hwk1_material/material_hw1/part02/filter_data.m
@@ -3,6 +3,19 @@ function yf = filter_data(y,coeff)
 % yf: filtered signal
 % y: original signal
 % coeff: coefficient of the FIR filter
-
+yf=zeros(length(y),1);
+for(n=1:1:length(y))
+    if(n>length(coeff))
+        for(k=1:1:length(coeff))
+            yf(n,1)=yf(n,1)+coeff(k)*y(n-k);
+        end
+    else
+        for(k=1:1:(n-1))
+            yf(n,1)=yf(n,1)+coeff(k)*y(n-k);
+        end
+    end
 end
+
+
+
 
--- Homework 1/SIS_21-22_hwk1_material/material_hw1/part02/sample_data.m
+++ Homework 1/SIS_21-22_hwk1_material/material_hw1/part02/sample_data.m
@@ -6,4 +6,12 @@ function [ts,ys]=sample_data(t,y,Fs)
 % y: original data
 % Fs: sampling frequency
 
+ts = zeros(round(length(y)/20),1);
+ys = zeros(round(length(y)/20),1);
+
+for i=1:1:round(length(y)/20)
+    ts(i) = t(i+20);
+    ys(i) = y(i+20);
+end
+
 end
